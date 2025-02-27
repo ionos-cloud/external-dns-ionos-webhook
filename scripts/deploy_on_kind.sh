4d596c6bb83a1835ac867b7d529a13af00b8a65c
Emilija Ivanovic <emilija.ivanovic@ionos.com>
2025-02-27 13:27:30 +0100

    Clean up deploy_on_kind.sh script

--- scripts/deploy_on_kind.sh
+++ scripts/deploy_on_kind.sh
@@ -6,25 +6,20 @@ set -e # exit on first error
 LOCAL_REGISTRY_PORT=5001
 LOCAL_REGISTRY_NAME=kind-registry
 LOCAL_REGISTRY_RUNNING=$(docker ps -a | grep -q $LOCAL_REGISTRY_NAME && echo "true" || echo "false")
-
 # docker
 IMAGE_REGISTRY=localhost:$LOCAL_REGISTRY_PORT
 IMAGE_NAME=external-dns-ionos-webhook
 IMAGE=$IMAGE_REGISTRY/$IMAGE_NAME
-
 #kind
 KIND_CLUSTER_NAME=external-dns
 KIND_CLUSTER_CONFIG=./deployments/kind/cluster.yaml
 KIND_CLUSTER_RUNNING=$(kind get clusters -q | grep -q $KIND_CLUSTER_NAME && echo "true" || echo "false")
 KIND_CLUSTER_WAIT=60s
-
 ## helm
 HELM_CHART=bitnami/external-dns
-HELM_RELEASE_NAME=ionos-external-dns
+HELM_RELEASE_NAME=external-dns
 HELM_VALUES_FILE=deployments/helm/local-kind-values.yaml
 
-HELM_CHART_REPO_URL=https://charts.bitnami.com/bitnami
-HELM_CHART_REPO_NAME=bitnami
 
 # if there is a clean up argument, delete the kind cluster and local registry
 if [ "$1" = "clean" ]; then
@@ -47,14 +42,31 @@ if [ "$1" = "helm-delete" ]; then
     exit 0
 fi
 
-printf "LOCAL_REGISTRY_RUNNING: %s\n" "$LOCAL_REGISTRY_RUNNING"
 printf "KIND_CLUSTER_RUNNING: %s\n" "$KIND_CLUSTER_RUNNING"
+printf "LOCAL_REGISTRY_RUNNING: %s\n" "$LOCAL_REGISTRY_RUNNING"
+
+# run kind cluster if not running
+if [ "$KIND_CLUSTER_RUNNING" = "false" ]; then
+    printf "Starting kind cluster...\n"
+    kind create cluster  --name $KIND_CLUSTER_NAME --config $KIND_CLUSTER_CONFIG
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+    sleep $KIND_CLUSTER_WAIT
+fi
 
 # run local registry if not running
 if [ "$LOCAL_REGISTRY_RUNNING" = "false" ]; then
     printf "Starting local registry...\n"
     docker run -d --restart=always -p "127.0.0.1:$LOCAL_REGISTRY_PORT:5000" --name "$LOCAL_REGISTRY_NAME" registry:2
 fi
+
+# when both the kind cluster and local registry are running, connect them and install dns mock server
+docker network connect "kind" "$LOCAL_REGISTRY_NAME"
+kubectl apply -f ./deployments/kind/local-registry-configmap.yaml
+printf "Installing dns mock server...\n"
+helm upgrade --install --namespace dns-mockserver --create-namespace dns-mockserver mockserver/mockserver -f ./deployments/dns-mockserver/dns-mockserver-values.yaml
+sleep 20
+curl -v -X PUT -H "Content-Type: application/json" http://dns-mockserver.127.0.0.1.nip.io/mockserver/expectation -d @scripts/expectation-payload.json
+
 
 printf "Building binary...\n"
 make build
@@ -64,20 +76,6 @@ make docker-build
 
 printf "Pushing image...\n"
 make docker-push
-
-# run kind cluster if not running
-if [ "$KIND_CLUSTER_RUNNING" = "false" ]; then
-    printf "Starting kind cluster...\n"
-    kind create cluster  --name $KIND_CLUSTER_NAME --config $KIND_CLUSTER_CONFIG
-    kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-    sleep $KIND_CLUSTER_WAIT
-    docker network connect "kind" "$LOCAL_REGISTRY_NAME"
-    kubectl apply -f ./deployments/kind/local-registry-configmap.yaml
-    printf "Installing dns mock server...\n"
-    helm upgrade --install --namespace dns-mockserver --create-namespace dns-mockserver mockserver/mockserver -f ./deployments/dns-mockserver/dns-mockserver-values.yaml
-    sleep 20
-    curl -v -X PUT -H "Content-Type: application/json" http://dns-mockserver.127.0.0.1.nip.io/mockserver/expectation -d @scripts/expectation-payload.json
-fi
 
 helm upgrade $HELM_RELEASE_NAME $HELM_CHART -f $HELM_VALUES_FILE --install
 
